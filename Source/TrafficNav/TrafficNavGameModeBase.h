d93f2be16c18a3c4d1c440422c77c86720b94ab5
Kshitij9921 <kshitijcobra@gmail.com>
2023-01-25 22:40:38 +0530

    Git init 2
    Git Init 2

---
+++ Source/TrafficNav/TrafficNavGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrafficNavGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAFFICNAV_API ATrafficNavGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
